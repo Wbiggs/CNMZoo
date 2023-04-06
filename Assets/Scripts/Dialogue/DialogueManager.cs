736e254d5999d91daa1a3abeef2bb03fd7ee9f3c
End3r6 <jswardson@cnm.edu>
2023-04-06 15:19:03 -0600

    Test Text and interface

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+
 public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager instance {get; private set; }
